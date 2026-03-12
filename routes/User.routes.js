692aaf41f13094318e1bdfd30c106fdf5af41cea
Fiote <brenoft14@gmail.com>
2026-03-12 09:43:39 -0300

    Adicionando rota de criação dos usuarios(master/clerk) - rotas completas

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import User from '../models/User.model.js'
 import Business from '../models/Business.model.js'
+import bcrypt from 'bcrypt'
 
 const user = express.Router()
 
+//Get de usuarios
 user.get('/', async (req, res) => {
     try {
         const Users = await User.find()
@@ -13,49 +15,65 @@ user.get('/', async (req, res) => {
     }
 })
 
+//Criação de Usuario
 user.post('/create', async (req, res) => {
 
-    const { name, email, password, role, businessName } = req.body
+    const { name, email, password, role, businessName, businessId } = req.body
 
     try {
-        let business
-        let businessId
+        let finalBusinessId = businessId;
+        const passwordHash = await bcrypt.hash(password, 12)
 
+        //nesse if eu tive que fazer todo o processo de criação para caso o usuario seja o "master" da empresa
         if (role === "master") {
 
-            business = await Business.create({
+            const newBusiness = await Business.create({
                 name: businessName
+            })
+            finalBusinessId = newBusiness._id
+
+
+            const newUser = await User.create({
+                name,
+                email,
+                password: passwordHash,
+                role,
+                businessId: finalBusinessId
+            })
+
+            newBusiness.userID = newUser._id
+            await newBusiness.save()
+
+            return res.status(200).json({
+                msg: "Usuário master e empresa criados!",
+                data: newUser
             })
 
 
-
-            businessId = business._id
         }
 
+        //Já aqui fora eu crio basicamente a mesma parte do processo de criação do "master" porem sem a utilização do businessName(propiedade atribuida somente a criação do usuario "master")
         const newUser = await User.create({
             name,
             email,
-            password,
+            password: passwordHash,
             role,
-            businessId
+            businessId: finalBusinessId
         })
-        
-        business.userID = newUser._id
-        await business.save()
-
-
         return res.status(201).json({
             msg: "Usuário criado com sucesso",
             data: newUser
         })
 
+
     } catch (error) {
         return res.status(500).json({
             msg: 'Erro no servidor tente novamente mais tarde',
-            error
+            error: error.message
         })
     }
 })
+
 
 
 export default user
